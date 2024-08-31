d559030cdfdecac97c1d0cb2571792e05e862354
HediCDS <162295896+HediCDS@users.noreply.github.com>
2024-08-31 18:28:17 +0200

    Implement a word generator function
    - Added a function to generate unique words which will be used for testing.
    - Imported 'random' package to help in generating the strings.

--- Test_BloomFilter.py
+++ Test_BloomFilter.py
@@ -1,3 +1,11 @@
 from Bloom_filter import BloomFilter
 import string
 from typing import List, Tuple
+import random
+
+def unique_words_generator(n: int, length: int) -> List:
+    words = set()
+    while len(words) < n:
+        word = ''.join(random.choices(string.ascii_lowercase, k=length))
+        words.add(word)
+    return list(words)
