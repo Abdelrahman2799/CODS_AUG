a9981e4c01307eb3681ede44c5483dfdf2833781
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-08-31 18:46:50 +0200

    Introduce tests for insertion and search in Test_BloomFilter.py
    Created two functions to test insertion and search:
    - 'test_insert': To test that all inserted elements are found in the Bloom filter.
    -'test_search': To test that the observed false positive rate is close to what is expected.

--- Test_BloomFilter.py
+++ Test_BloomFilter.py
@@ -9,3 +9,36 @@ def unique_words_generator(n: int, length: int) -> List:
         word = ''.join(random.choices(string.ascii_lowercase, k=length))
         words.add(word)
     return list(words)
+
+def test_insert(exp_count: int, fp_rate: float, words_length:int=10):
+    add_words = unique_words_generator(exp_count, words_length)
+    bf = BloomFilter(exp_count, fp_rate)
+    for word in add_words:
+        bf.insert(word)
+
+    for word in add_words:
+        assert bf.search(word), f"'{word}' was not found even though it was inserted!"
+    
+
+def test_search(exp_count: int, fp_rate: float, words_length:int=10, size:int=10000) -> float:
+    add_words = unique_words_generator(exp_count, words_length)
+    to_test = unique_words_generator(size, words_length)
+
+    to_test = [word for word in to_test if word not in add_words]
+    bloom = BloomFilter(exp_count, fp_rate)
+    for word in add_words:
+        bloom.insert(word)
+    fp_count = 0
+    for word in to_test:
+        if bloom.search(word):
+            fp_count += 1
+    
+    observed_fp_rate = fp_count / len(to_test)
+    print(f'Expected FPR: {fp_rate}, Observed FPR: {observed_fp_rate}')
+
+    return observed_fp_rate
+
+
+
+
+
