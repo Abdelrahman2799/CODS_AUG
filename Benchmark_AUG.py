e5ad8ef9ce3c567275feafc9b43a22420f83373b
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-08-31 21:27:39 +0200

    Initialize Bloom and compute array size and hash counts
    - Set the parameters needed for the bloom filter.
    - Initialized a bloom filter with defined parameters.
    - Computed the array size and the number of hash functions.
    - These were added to be used in the evaluation process.

--- Benchmark_AUG.py
+++ Benchmark_AUG.py
@@ -13,3 +13,17 @@ def unique_words_generator(n, length):
         words.add(word)
     return list(words)
 
+exp_count = 100000
+fp_rate = 0.01
+word_length = 10
+fpr_test_size = 10000
+repetition = 10
+steps = 10000 
+
+bf = BloomFilter(exp_count, fp_rate)
+
+m = bf.compute_size(exp_count, fp_rate)
+k = bf.compute_hash_count(m, exp_count)
+
+
+
