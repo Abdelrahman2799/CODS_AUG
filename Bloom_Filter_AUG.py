fbf3f98506d8d82970422a43dc108e133d83602d
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-08-31 15:09:59 +0200

    Add method to calculate the number of hash functions
    implemented 'compute_hash_count' method to compute the optimal number of hash functions (k) in the BloomFilter:
    - calculates number of hash functions using array size and expected elements following this formula k = (arraysize / expected number of elements) * ln(2)
    - This to ensure that the Bloom filter minimizes false positive rate while optimizing space efficiency.

--- Bloom_Filter_AUG.py
+++ Bloom_Filter_AUG.py
@@ -10,8 +10,12 @@ class BloomFilter:
         self.size = self.compute_size(exp_count, self.fp_rate)
         self.array = bitarray(self.size)
         self.array.setall(0)
+        self.hfunctions = self.compute_hash_count(self.size, exp_count)
         
     def compute_size(self, count, prob) -> int:
         size = - (count * math.log(prob)) / (math.log(2) ** 2)
         return int(size)
 
+    def compute_hash_count(self, arraysize: int, exp_words: int) -> int:
+        hash_count = (arraysize / exp_words) * math.log(2)
+        return int(hash_count)
