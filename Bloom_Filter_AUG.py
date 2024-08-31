7718eb042008e9472e3712bddc7494c1d56db362
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-08-31 14:55:12 +0200

    Add bit array in BloomFilter __init__method
    - Introduced bit array as a class attribute with the calculated size.
    - Filled the array with zeros, making it ready for insertions and searches.

--- Bloom_Filter_AUG.py
+++ Bloom_Filter_AUG.py
@@ -8,9 +8,10 @@ class BloomFilter:
     def __init__(self, exp_count: int, fp_rate: float):
         self.fp_rate = fp_rate
         self.size = self.compute_size(exp_count, self.fp_rate)
-      
+        self.array = bitarray(self.size)
+        self.array.setall(0)
+        
     def compute_size(self, count, prob) -> int:
         size = - (count * math.log(prob)) / (math.log(2) ** 2)
         return int(size)
-
 
