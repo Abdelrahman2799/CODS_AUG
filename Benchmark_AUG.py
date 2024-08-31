f4ceee49fb47cd7c1fdf5b4119f936214cc78c94
HediCDS <162295896+HediCDS@users.noreply.github.com>
2024-08-31 21:16:22 +0200

    Add unique words generator function to Benchmark_AUG.py
    Used the unique words generator function to generate unique words which will be used in evaluation.

--- Benchmark_AUG.py
+++ Benchmark_AUG.py
@@ -4,3 +4,12 @@ import string
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def unique_words_generator(n, length):
+    words = set()
+    while len(words) < n:
+        word = ''.join(random.choices(string.ascii_lowercase, k=length))
+        words.add(word)
+    return list(words)
+
